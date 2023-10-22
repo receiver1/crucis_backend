91dd7af70c0dae79ac9f48ba2c590d6d102703fd
Aleksander Kot <receiver8@yandex.ru>
2023-10-22 19:56:41 +0400

    fix user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,4 +20,9 @@ class Comment extends Model
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
