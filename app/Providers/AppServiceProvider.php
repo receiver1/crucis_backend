2e908bf7dce30da2a1fb18b6b5b1adcd3adcd12e
Aleksander Kot <receiver8@yandex.ru>
2023-10-23 11:57:46 +0400

    fix docs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        Scramble::routes(function (Route $route) {
+            return Str::startsWith($route->uri, 'v1/');
+        });
 
         Scramble::extendOpenApi(function (OpenApi $openApi) {
             $openApi->secure(
--- config/scramble.php
+++ config/scramble.php
@@ -7,7 +7,7 @@ return [
      * Your API path. By default, all routes starting with this path will be added to the docs.
      * If you need to change this behavior, you can add your custom routes resolver using `Scramble::routes()`.
      */
-    'api_path' => '',
+    'api_path' => null,
 
     /*
      * Your API domain. By default, app domain is used. This is also a part of the default API routes
