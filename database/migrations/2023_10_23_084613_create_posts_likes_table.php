a890fff69058f8915ea5e13084b35bfdc4a2d2a7
Aleksander Kot <receiver8@yandex.ru>
2023-10-26 00:23:15 +0400

    add cascade on posts_likes

--- database/migrations/2023_10_23_084613_create_posts_likes_table.php
+++ database/migrations/2023_10_23_084613_create_posts_likes_table.php
@@ -12,9 +12,9 @@ return new class extends Migration {
     {
         Schema::create('posts_likes', function (Blueprint $table) {
             $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->bigInteger('post_id');
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->primary(['user_id', 'post_id']);
         });
     }
